9079646ea68011b1dc3c0c56188d66495fcf8095
Arthur Soares e Silva Pereira de Miranda <160657905+Arthursoares2008@users.noreply.github.com>
2024-07-10 10:20:39 -0300

    Update Inicio.kt

--- jogo da velha_new/app/src/main/java/com/example/jogodavelha/Inicio.kt
+++ jogo da velha_new/app/src/main/java/com/example/jogodavelha/Inicio.kt
@@ -8,21 +8,40 @@ import com.example.jogodavelha.databinding.AcivityInicioBinding
 import com.example.jogodavelha.databinding.ActivityMainBinding
 
 class Inicio : AppCompatActivity() {
-    private lateinit var binding:AcivityInicioBinding
 
+    // Declaração da propriedade binding com late-initialization
+    // Será usada para acessar os elementos da interface do usuário
+    private lateinit var binding: ActivityInicioBinding
+
+    // Método onCreate é chamado quando a atividade é criada
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = AcivityInicioBinding.inflate(layoutInflater)
+        // Inicializa o objeto binding com o layout inflater
+        // Permite acessar os elementos de layout associados ao ActivityInicioBinding
+        binding = ActivityInicioBinding.inflate(layoutInflater)
+
+        // Chama o método onCreate da classe pai (AppCompatActivity)
+        // Isso deve ser feito após a inicialização do binding para garantir que a atividade seja configurada corretamente
         super.onCreate(savedInstanceState)
+
+        // Define o layout da atividade para o root do binding
+        // Isso conecta a hierarquia de visualização com a atividade
         setContentView(binding.root)
 
+        // Configura um clique para o botão nivel1
         binding.nivel1.setOnClickListener {
+            // Cria uma ação para iniciar a MainActivity
             val intent = Intent(this, MainActivity::class.java)
 
+            // Inicia a atividade especificada pela intenção
             startActivity(intent)
         }
+
+        // Configura um clique para o botão nivel2
         binding.nivel2.setOnClickListener {
+            // Cria uma ação para iniciar a MainActivity2
             val intent = Intent(this, MainActivity2::class.java)
 
+            // Inicia a atividade especificada pela ação
             startActivity(intent)
         }
     }
