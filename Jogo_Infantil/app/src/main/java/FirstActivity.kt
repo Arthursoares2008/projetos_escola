ca9d7312a54e806d238bc2264c7114d2b7b975dd
Arthur Soares e Silva Pereira de Miranda <160657905+Arthursoares2008@users.noreply.github.com>
2024-08-22 23:18:56 -0300

    Update FirstActivity.kt

--- Jogo_Infantil/app/src/main/java/FirstActivity.kt
+++ Jogo_Infantil/app/src/main/java/FirstActivity.kt
@@ -7,17 +7,22 @@ import com.example.game_english.databinding.ActivityFirstBinding
 
 class FirstActivity : AppCompatActivity() {
 
+    // Variável de binding para acessar as views do layout.
     private lateinit var binding: ActivityFirstBinding
 
+    // Método chamado quando a atividade é criada.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // Infla o layout usando View Binding e configura o conteúdo da atividade.
         binding = ActivityFirstBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configura o clique do botão para iniciar MainActivity.
         binding.btnStartGame.setOnClickListener {
-
+            // Cria um Intent para iniciar MainActivity.
             val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            startActivity(intent)  // Inicia MainActivity.
         }
     }
 }
